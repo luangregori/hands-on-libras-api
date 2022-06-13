e1f5935d7650ff85d3062ddbf46446ff3bd4e58d
Luan de Gregori <luan.gregori@banqi.com>
2022-06-13 13:29:31 -0300

    feat: implements FindAllCategoriesRepository

---
+++ src/infra/db/mongodb/category-repository/category.ts
@@ -0,0 +1,11 @@
+import { FindAllCategoriesRepository } from '../../../../data/protocols/find-all-categories-repository'
+import { CategoryModel } from '../../../../domain/models/category'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class CategoryMongoRepository implements FindAllCategoriesRepository {
+  async findAll (): Promise<CategoryModel[]> {
+    const categoryCollection = await MongoHelper.getCollection('categories')
+    const result = await categoryCollection.find().toArray()
+    return result.map(category => MongoHelper.map(category))
+  }
+}
