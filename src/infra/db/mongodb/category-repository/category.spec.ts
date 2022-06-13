8c7229084f719dd646ef615a4d2c76cf844687a0
Luan de Gregori <luan.gregori@banqi.com>
2022-06-13 13:29:46 -0300

    test: implements FindAllCategoriesRepository

---
+++ src/infra/db/mongodb/category-repository/category.spec.ts
@@ -0,0 +1,37 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { CategoryMongoRepository } from './category'
+
+let categoryCollection: Collection
+
+describe('Category Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    categoryCollection = await MongoHelper.getCollection('categories')
+    await categoryCollection.deleteMany({})
+  })
+
+  const makeSut = (): CategoryMongoRepository => {
+    return new CategoryMongoRepository()
+  }
+
+  test('Should return all categories', async () => {
+    await categoryCollection.insertOne({
+      id: 'valid_id',
+      name: 'valid_name'
+    })
+    const sut = makeSut()
+    const categories = await sut.findAll()
+    expect(categories).toBeDefined()
+    expect(categories.length).toBe(1)
+    expect(categories[0].id).toBeDefined()
+    expect(categories[0].name).toBe('valid_name')
+  })
+})
