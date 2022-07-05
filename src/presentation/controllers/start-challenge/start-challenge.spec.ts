d02fb4dbc90f62b9c1e8b36f317f005c49477e0a
Luan de Gregori <luan.gregori@banqi.com>
2022-07-05 10:53:13 -0300

    feat: create controller

---
+++ src/presentation/controllers/start-challenge/start-challenge.spec.ts
@@ -0,0 +1,83 @@
+import { MissingParamError, ServerError } from '@/presentation/errors'
+import { badRequest, ok, serverError } from '@/presentation/helpers/http-helper'
+import { StartChallengeController } from './start-challenge'
+import { StartChallenge } from './start-challenge-protocols'
+
+interface SutTypes {
+  sut: StartChallengeController
+  startChallengeStub: StartChallenge
+}
+
+const makeSut = (): SutTypes => {
+  const startChallengeStub = makeStartChallengeStub()
+  const sut = new StartChallengeController(startChallengeStub)
+  return {
+    sut,
+    startChallengeStub
+  }
+}
+
+const makeStartChallengeStub = (): StartChallenge => {
+  class StartChallengeStub implements StartChallenge {
+    async start (params: StartChallenge.Params): Promise<StartChallenge.Result> {
+      return await new Promise(resolve => resolve(makeFakeStartChallengeResult()))
+    }
+  }
+  return new StartChallengeStub()
+}
+
+const makeFakeStartChallengeResult = (): StartChallenge.Result => ({
+  challengeInfo: {
+    id: 'valid_id',
+    name: 'valid_name',
+    description: 'valid_description',
+    image_url: 'valid_image',
+    categoryId: 'valid_category_id'
+  },
+  userInfo: {
+    completed: true,
+    score: 1
+  }
+})
+
+const makeFakeRequest = (): StartChallengeController.Request => ({
+  challengeId: 'valid_id',
+  accountId: 'valid_account_id'
+})
+
+describe('StartChallenge Controller', () => {
+  test('Should return 400 if no challengeId is provided', async () => {
+    const { sut } = makeSut()
+    const request = {
+      challengeId: null,
+      accountId: 'valid_account_id'
+    }
+    const httpResponse = await sut.handle(request)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('challengeId')))
+  })
+
+  test('Should call StartChallenge UseCase with correct values', async () => {
+    const { sut, startChallengeStub } = makeSut()
+    const startSpy = jest.spyOn(startChallengeStub, 'start')
+    await sut.handle(makeFakeRequest())
+    expect(startSpy).toHaveBeenCalledWith({
+      challengeId: 'valid_id',
+      accountId: 'valid_account_id'
+    })
+  })
+
+  test('Should return 500 if StartChallenge UseCase throws', async () => {
+    const { sut, startChallengeStub } = makeSut()
+    jest.spyOn(startChallengeStub, 'start').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new ServerError(null)))
+  })
+
+  test('Should return 200 if valid data is provided', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(ok(makeFakeStartChallengeResult()))
+  })
+})
