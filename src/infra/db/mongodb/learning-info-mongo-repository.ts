3f32d7838bbf0e5a3c3a0006b08217fb6a3cd4af
Luan de Gregori <luan.gregori@banqi.com>
2022-08-25 15:57:12 -0300

    feat: add infra to learn challenge

---
+++ src/infra/db/mongodb/learning-info-mongo-repository.ts
@@ -0,0 +1,11 @@
+import { FindLearningInfoRepository } from '@/data/protocols'
+import { LearningInfoModel } from '@/domain/models'
+import { MongoHelper } from '@/infra/db'
+
+export class LearningInfoMongoRepository implements FindLearningInfoRepository {
+  async findByChallengeId (challengeId: string): Promise<LearningInfoModel[]> {
+    const learningInfoCollection = await MongoHelper.getCollection('learning-infos')
+    const result = await learningInfoCollection.find({ challengeId }).toArray()
+    return result.length ? result.map(el => MongoHelper.map(el)) : result
+  }
+}
