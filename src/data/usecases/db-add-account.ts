6c7579eb0e72b8d542e39272ae7222ab8458badc
Luan de Gregori <luan.gregori@banqi.com>
2022-09-29 11:31:25 -0300

    fix: lint ant start script

--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -3,12 +3,12 @@ import { AddAccount } from '@/domain/usecases'
 import { AccountModel } from '@/domain/models'
 
 export class DbAddAccount implements AddAccount {
-  constructor(
+  constructor (
     private readonly hasher: Hasher,
     private readonly addAccountRepository: AddAccountRepository
   ) { }
 
-  async add(accountData: AddAccount.Params): Promise<AccountModel> {
+  async add (accountData: AddAccount.Params): Promise<AccountModel> {
     const hashedPassword = await this.hasher.hash(accountData.password)
     const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
     return account
