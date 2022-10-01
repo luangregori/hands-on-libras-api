d7e12966b34139bf273daa9c293e2e63d93a6eb5
Luan de Gregori <luan.gregori@banqi.com>
2022-09-30 22:58:53 -0300

    feat: add route to update user info

--- tests/presentation/controllers/user-info.spec.ts
+++ tests/presentation/controllers/user-info.spec.ts
@@ -2,20 +2,23 @@ import faker from 'faker'
 import { UserInfoController } from '@/presentation/controllers'
 import { ok, serverError } from '@/presentation/helpers/http-helper'
 import { ServerError } from '@/presentation/errors'
-import { LoadUserInfoSpy } from '@/tests/presentation/mocks'
+import { LoadUserInfoSpy, LoadUserScoreSpy } from '@/tests/presentation/mocks'
 import { throwError } from '@/tests/domain/mocks'
 
 interface SutTypes {
   sut: UserInfoController
-  loadUserInfoSPy: LoadUserInfoSpy
+  loadUserInfoSpy: LoadUserInfoSpy
+  loadUserScoreSpy: LoadUserScoreSpy
 }
 
 const makeSut = (): SutTypes => {
-  const loadUserInfoSPy = new LoadUserInfoSpy()
-  const sut = new UserInfoController(loadUserInfoSPy)
+  const loadUserInfoSpy = new LoadUserInfoSpy()
+  const loadUserScoreSpy = new LoadUserScoreSpy()
+  const sut = new UserInfoController(loadUserInfoSpy, loadUserScoreSpy)
   return {
     sut,
-    loadUserInfoSPy
+    loadUserInfoSpy,
+    loadUserScoreSpy
   }
 }
 
@@ -25,23 +28,26 @@ const mockRequest = (): UserInfoController.Request => ({
 
 describe('User Info Controller', () => {
   test('Should call LoadUserInfo with correct valuesS', async () => {
-    const { sut, loadUserInfoSPy } = makeSut()
-    const loadSpy = jest.spyOn(loadUserInfoSPy, 'load')
+    const { sut, loadUserInfoSpy } = makeSut()
+    const loadSpy = jest.spyOn(loadUserInfoSpy, 'load')
     const request = mockRequest()
     await sut.handle(request)
     expect(loadSpy).toHaveBeenCalledWith(request.accountId)
   })
 
   test('Should return 500 if LoadUserInfo throws', async () => {
-    const { sut, loadUserInfoSPy } = makeSut()
-    jest.spyOn(loadUserInfoSPy, 'load').mockImplementationOnce(throwError)
+    const { sut, loadUserInfoSpy } = makeSut()
+    jest.spyOn(loadUserInfoSpy, 'load').mockImplementationOnce(throwError)
     const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(serverError(new ServerError(null)))
   })
 
   test('Should return 200 if valid data is provided', async () => {
-    const { sut, loadUserInfoSPy } = makeSut()
+    const { sut, loadUserInfoSpy, loadUserScoreSpy } = makeSut()
     const httpResponse = await sut.handle(mockRequest())
-    expect(httpResponse).toEqual(ok(loadUserInfoSPy.result))
+    expect(httpResponse).toEqual(ok({
+      userInfo: loadUserInfoSpy.result,
+      userScore: loadUserScoreSpy.result
+    }))
   })
 })
