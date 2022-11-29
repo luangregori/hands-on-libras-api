72c429c97c5187d1c9cea67e126a3a0446b54860
Luan de Gregori <luan.gregori@banqi.com>
2022-11-29 11:29:02 -0300

    fix: :bug: dont update password and id

--- src/data/usecases/db-update-account.ts
+++ src/data/usecases/db-update-account.ts
@@ -1,17 +1,18 @@
 import { UpdateAccount } from '@/domain/usecases'
 import { FindAccountRepository, HashComparer, Hasher, UpdateAccountRepository } from '@/data/protocols'
+import { AccountModel } from '@/domain/models'
 
 export class DbUpdateAccount implements UpdateAccount {
-  constructor (
+  constructor(
     private readonly findAccountRepository: FindAccountRepository,
     private readonly hashComparer: HashComparer,
     private readonly updateAccountRepository: UpdateAccountRepository,
     private readonly hasher: Hasher
   ) { }
 
-  async updateById (accountId: string, params: UpdateAccount.Params): Promise<UpdateAccount.Result> {
+  async updateById(accountId: string, params: UpdateAccount.Params): Promise<UpdateAccount.Result> {
+    const account = await this.findAccountRepository.findById(accountId)
     if (params.newPassword) {
-      const account = await this.findAccountRepository.findById(accountId)
       const isValid = await this.hashComparer.compare(params.oldPassword, account.password)
       if (!isValid) {
         throw new Error('Invalid password')
@@ -21,9 +22,17 @@ export class DbUpdateAccount implements UpdateAccount {
       delete params.oldPassword
       delete params.newPassword
     }
+    params = this.checkNewEmail(account, params)
 
     const updatedAccount = await this.updateAccountRepository.updateById(accountId, params)
     delete updatedAccount.password
     return updatedAccount
+  }
+
+  private checkNewEmail(account: AccountModel, params: UpdateAccount.Params): UpdateAccount.Params {
+    if (params.email && params.email !== account.email) {
+      params.email_verified = false
+    }
+    return params
   }
 }
--- src/presentation/controllers/update-user-info.ts
+++ src/presentation/controllers/update-user-info.ts
@@ -4,14 +4,21 @@ import { UpdateAccount } from '@/domain/usecases'
 import { MissingParamError } from '../errors'
 
 export class UpdateUserInfoController implements Controller {
-  constructor (
+  constructor(
     private readonly updateAccount: UpdateAccount
   ) { }
 
-  async handle (request: UpdateUserInfoController.Request): Promise<HttpResponse> {
+  async handle(request: UpdateUserInfoController.Request): Promise<HttpResponse> {
     try {
       if (request.newPassword && !request.oldPassword) {
         return badRequest(new MissingParamError('oldPassword'))
+      }
+
+      const protectedFields = ["id", "_id", "password"]
+      for (const field of protectedFields) {
+        if (request[field]) {
+          delete request[field]
+        }
       }
 
       const { accountId, ...params } = request
@@ -30,5 +37,6 @@ export namespace UpdateUserInfoController {
     image_url?: string
     oldPassword?: string
     newPassword?: string
+    password?: string
   }
 }
